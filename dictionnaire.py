d07505df6b1e208f00831d0ee494ad8a3f8ad2ec
EmilienReuillard <emilien.reuillard@outlook.com>
2023-12-10 22:30:10 +0100

    génération de mot aléatoire

--- dictionnaire.py
+++ dictionnaire.py
@@ -1,4 +1,5 @@
 from wiktionaryparser import WiktionaryParser
+import random
 
 def obtenir_definition_wiktionnaire(mot):
     parser = WiktionaryParser()
@@ -11,8 +12,22 @@ def obtenir_definition_wiktionnaire(mot):
     
     return(mot_info[0]['definitions'][0]['text'][1])
 
+def charger_mots_francais(path):
+    with open(path, 'r', encoding='utf-8') as fichier:
+        mots = fichier.read().splitlines()
+    return mots
+
+def generer_mot_francais_aleatoire(path_dictionnary):
+    mots_francais = charger_mots_francais(path_dictionnary)
+    mot_aleatoire = random.choice(mots_francais)
+    return mot_aleatoire
+
 if __name__ == "__main__":
-    # Exemple d'utilisation
-    mot_a_chercher = "pouvoir"
-    definition = obtenir_definition_wiktionnaire(mot_a_chercher)
-    print(f"{mot_a_chercher}: {definition}")
+    
+    # Génération d'un mot aléatoire
+    random_word = generer_mot_francais_aleatoire('database/dico.txt')
+    print(random_word)
+    
+    # recherche de définition
+    definition = obtenir_definition_wiktionnaire(random_word)
+    print(f"{random_word}: {definition}")
